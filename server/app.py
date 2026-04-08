5d2fba948925019e89e261127cd922926da1714b
Shaurya1901 <shauryasinghal1901@gmail.com>
2026-04-08 16:39:06 +0000

    fix: correct create_app arguments

--- server/app.py
+++ server/app.py
@@ -1,12 +1,11 @@
-
 from fastapi import FastAPI
 from openenv.core.env_server.http_server import create_app
 from hydropower_dam_env_environment import HydropowerDamEnvEnvironment
 from models import HydropowerAction, HydropowerObservation, HydropowerState
 
 app: FastAPI = create_app(
-    environment_class=HydropowerDamEnvEnvironment,
-    action_class=HydropowerAction,
-    observation_class=HydropowerObservation,
-    state_class=HydropowerState,
+    env=HydropowerDamEnvEnvironment,
+    action_cls=HydropowerAction,
+    observation_cls=HydropowerObservation,
+    max_concurrent_envs=1,
 )
